bc87fc8e43ec2cadf98f457cb33746ed6db7dae5
matheusslongo <146895466+Matheusgta56@users.noreply.github.com>
2023-12-12 22:45:42 +0000

    0001

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h1 className='text-center'>Minha Avaliação Substitutiva</h1>
+      <h1 className='text-center'>Minha Avaliação Substitutivaasdas</h1>
       
     </div>
   )
